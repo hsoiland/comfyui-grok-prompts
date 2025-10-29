0da482373577036442504ff8811e888402cd490a
hsoiland <harrysoiland@gmail.com>
2025-10-29 23:39:54 +1100

    audio

--- __init__.py
+++ __init__.py
@@ -6,10 +6,12 @@ else:
     from ponyxl import PonyXL
     from flux import Flux
     from wan import Wan
+    from audio import Audio
 
 NODE_CLASS_MAPPINGS = {
     "PonyXL": PonyXL,
     "Flux": Flux,
-    "Wan": Wan
+    "Wan": Wan,
+    "Audio": Audio
 }
 print("\033[34mGrok Prompts: \033[92mLoaded\033[0m")
