4e43c920064d4679bd00b8394ebc8420cf5cff34
hsoiland <harrysoiland@gmail.com>
2025-10-29 18:42:29 +1100

    add wan

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 if __package__:
     from .ponyxl import PonyXL
     from .flux import Flux
+    from .wan import Wan
 else:
     from ponyxl import PonyXL
     from flux import Flux
-
+    from wan import Wan
 NODE_CLASS_MAPPINGS = {
     "PonyXL": PonyXL,
-    "Flux": Flux
+    "Flux": Flux,
+    "Wan": Wan
 }
 print("\033[34mGrok Prompts: \033[92mLoaded\033[0m")
